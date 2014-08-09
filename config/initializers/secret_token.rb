159a4982f70d39df5b2912762f2954f28a0bb7b9
prijeni <jeniss37@gmail.com>
2014-08-09 12:06:57 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleBugTracker::Application.config.secret_token = '48a0b5d1baad8f847c427b871e7100ee801e041ced8ac22417021cb7e5bc75b6f6f8cd1248e9741a699936976a439aa5def8971954feec115a592f330d8786a1'
